a64008ba9f0cc909f61a368621fc147e9bf7a198
Frazer McLean <frazer@frazermclean.co.uk>
2019-12-06 23:37:18 +0100

    clean up day 4

--- aoc/day04.py
+++ aoc/day04.py
@@ -2,7 +2,7 @@ from itertools import groupby
 
 
 def exactly_two_adjacent(iterable):
-    for k, g in groupby(iterable):
+    for _, g in groupby(iterable):
         if sum(1 for _ in g) == 2:
             return True
     return False
@@ -18,40 +18,20 @@ def is_sorted(seq):
     return seq == sorted(seq)
 
 
-def part1(lo, hi):
-    num = 0
-
-    for x in range(lo, hi + 1):
-        digits = list(str(x))
-        if not is_sorted(digits):
-            continue
-
-        if is_unique(digits):
-            continue
-
-        num += 1
-
-    return num
+def check_valid1(password):
+    chars = list(password)
+    return is_sorted(chars) and not is_unique(chars)
 
 
-def part2(lo, hi):
-    num = 0
-
-    for i in range(lo, hi + 1):
-        digits = list(str(i))
-
-        if not is_sorted(digits):
-            continue
-
-        if not exactly_two_adjacent(digits):
-            continue
-
-        num += 1
-
-    return num
+def check_valid2(password):
+    chars = list(password)
+    return is_sorted(chars) and exactly_two_adjacent(chars)
 
 
 def solve(file, verbose):
     lo, hi = map(int, file.read().split('-'))
-    print('Part 1:', part1(lo, hi))
-    print('Part 2:', part2(lo, hi))
+
+    passwords = [str(p) for p in range(lo, hi + 1)]
+
+    print('Part 1:', sum(1 for p in passwords if check_valid1(p)))
+    print('Part 2:', sum(1 for p in passwords if check_valid2(p)))
